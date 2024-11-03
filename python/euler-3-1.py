c20b5a003322f9342b1977fd1f78c626b4d3c08f
masojopa <collaojorge52@hotmail.com>
2024-11-03 13:37:20 -0300

    euler-3.py modificado

--- python/euler-3-1.py
+++ python/euler-3-1.py
@@ -5,11 +5,15 @@ def exponent():
     
     prime=[]
     primos=[]
-    for i in range(1,20):
-        for j in range(1,20):
-            #print (i%j)
-            if (i%j)==0:
-                 prime.append(i)
-
-        print(prime)
+    n=631
+    for i in range(2,n+1):
+        primos=True
+        for j in range(2,int(n**0.5)+1):
+            x=(i%j==0)
+            if x:
+               primos=False
+               break 
+        if primos:
+            prime.append(i)
+    print(prime)
 exponent()
--- python/euler-3.py
+++ python/euler-3.py
@@ -1,20 +1,21 @@
 #Jorge_Collao
-#Larges Prime number up to agiven digit
+#Larges Prime factot of a given digit
 
-def exponent():
+def factores(x):
     
-    x=int(input("Enter x,\n"))
-    y=int((x//2)+1)
-    prime=[]
-    for i in range(1,x):
-        for j in range(1,y):
-            #print(i,j)
-            a= (i%j)
-            b= (i//j)
-            if a==0 and b!=1 and b!=i:
-                pass
-            else:
-                prime.append(i)
-            print(prime)
+    factor=[]
+    i=1
+    while i <= x:
+        if x%i==0:
+            factor.append(i)
+        i=i+1    
+    return(factor)
 
-exponent()
+def main():
+    y=int(input("Ingrese un Numero para calcular sus factores "))
+    factores(y)
+    print(y)
+
+main()
+
+
