3d5afaa56a3d4806659061d5e8df1b604436f826
masojopa <collaojorge52@hotmail.com>
2024-11-05 01:03:48 -0300

    Largest prime factor of a given number finished

---
+++ python/euler-3-2.py
@@ -0,0 +1,28 @@
+#Jorge_Collao
+#Larges Prime factor of a given digit
+
+def factores(x):
+    
+    factor=[]
+    for i in range(1,x+1):
+        if x%i==0:
+            factor.append(i)
+    return(factor)
+
+def primo(y):
+    
+    prim=[]
+    for j in y:
+        if len(factores(j))==2:
+            prim.append(j)
+    return(prim)
+
+
+def main():
+
+    lf=int(input("Ingrese un Numero para calcular sus factores "))
+    print(primo(factores(lf)))
+
+main()
+
+
